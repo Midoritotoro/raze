8b2939d77c88afe974acfa661d554288c3167327
Midoritotoro <danya20090504@gmail.com>
2026-06-17 14:24:54 +0300

    adjacent_find

--- include/raze/algorithm/find/AdjacentFind.h
+++ include/raze/algorithm/find/AdjacentFind.h
@@ -1,67 +1,206 @@
 #pragma once 
 
-#include <src/raze/algorithm/unchecked/find/AdjacentFindUnchecked.h>
 #include <raze/concurrency/Execution.h>
+#include <src/raze/algorithm/RangesSize.h>
+#include <src/raze/algorithm/VectorizablePredicate.h>
+#include <src/raze/algorithm/EqualTo.h>
+#include <src/raze/algorithm/NotFn.h>
+#include <src/raze/vx/dispatch/SizedSimdDispatcher.h>
+#include <raze/options/Options.h>
 
 
 __RAZE_ALGORITHM_NAMESPACE_BEGIN
 
-template <
-    class _ForwardIterator_,
-    class _BinaryPredicate_>
-__simd_nodiscard_inline_constexpr _ForwardIterator_ adjacent_find(
-    _ForwardIterator_   __first,
-    _ForwardIterator_   __last,
-    _BinaryPredicate_   __predicate) noexcept(
-        std::is_nothrow_invocable_v<
-            _BinaryPredicate_, 
-            std::iter_value_t<_ForwardIterator_>, 
-            std::iter_value_t<_ForwardIterator_>>)
-{
-    __verify_range(__first, __last);
+template <class _Traits_>
+struct _Adjacent_find : _Traits_ {
+	template <class _Iterator_, class _Sentinel_, class _Predicate_, class _Projection_>
+	struct __impl {
+		_Iterator_ _iterator;
+		_Sentinel_ _sentinel;
+		_Predicate_ _predicate;
+		_Projection_ _proj;
 
-    __seek_possibly_wrapped_iterator(__first, __adjacent_find_unchecked(
-        __unwrap_iterator(__first), __unwrap_iterator(__last),
-        type_traits::__pass_function(__predicate)));
+		constexpr explicit __impl(_Iterator_ __it, _Sentinel_ __sent, _Predicate_ __pred, _Projection_ __proj) noexcept :
+			_iterator(__it), _sentinel(__sent), _predicate(__pred), _proj(__proj)
+		{}
 
-    return __first;
-}
+		template <class _Tag_>
+		raze_always_inline raze_nodiscard constexpr bool operator()(_Tag_) noexcept {
+			if (_iterator == _sentinel) return true;
 
-template <class _ForwardIterator_>
-__simd_nodiscard_inline_constexpr _ForwardIterator_ adjacent_find(
-    _ForwardIterator_ __first,
-    _ForwardIterator_ __last) noexcept
-{
-    return raze::algorithm::adjacent_find(__first, __last, type_traits::equal_to<>{});
-}
+			auto __next = _iterator;
+			++__next;
 
-template <
-    class _ExecutionPolicy_,
-    class _ForwardIterator_,
-    concurrency::enable_if_execution_policy<_ExecutionPolicy_> = 0>
-__simd_nodiscard_inline _ForwardIterator_ adjacent_find(
-    _ExecutionPolicy_&&,
-    _ForwardIterator_   __first,
-    _ForwardIterator_   __last) noexcept
-{
-    return raze::algorithm::adjacent_find(__first, __last);
-}
+			if (__next == _sentinel) {
+				++_iterator;
+				return true;
+			}
+			else if (_predicate(_proj(*_iterator), _proj(*__next))) return true;
+			else {
+				++_iterator;
+				return false;
+			}
+		}
 
-template <
-    class _ExecutionPolicy_, 
-    class _ForwardIterator_, 
-    class _BinaryPredicate_>
-__simd_nodiscard_inline _ForwardIterator_ adjacent_find(
-    _ExecutionPolicy_&&,
-    _ForwardIterator_ __first, 
-    _ForwardIterator_ __last,
-    _BinaryPredicate_ __predicate) noexcept(
-        std::is_nothrow_invocable_v<
-            _BinaryPredicate_, 
-            std::iter_value_t<_ForwardIterator_>, 
-            std::iter_value_t<_ForwardIterator_>>)
-{
-    return raze::algorithm::adjacent_find(__first, __last, type_traits::__pass_function(__predicate));
-}
+		template <vx::simd_type _Tag_>
+		raze_nodiscard raze_always_inline void operator()(_Tag_, sizetype __aligned_size, sizetype __tail_size) noexcept {
+			auto* __ptr = std::to_address(_iterator);
+			auto* __end = __bytes_pointer_offset(__ptr, __aligned_size + __tail_size);
+
+			raze_assume(__ptr != __nullptr);
+
+			const auto __aligned_end = __bytes_pointer_offset(__ptr, __aligned_size);
+			auto __next_ptr = __bytes_pointer_offset(__ptr, sizeof(typename _Tag_::value_type));
+
+			do {
+				const auto __current = _proj(raze::vx::load<_Tag_>(__ptr));
+				const auto __next = _proj(raze::vx::load<_Tag_>(__next_ptr));
+				
+				const auto __mask = _predicate(__current, __next);
+
+				if (raze::vx::any_of(__mask)) {
+					__seek_possibly_wrapped_iterator(_iterator, __ptr + raze::vx::find_first_set(__mask));
+					return;
+				}
+
+				__advance_bytes(__ptr, sizeof(_Tag_));
+				__advance_bytes(__next_ptr, sizeof(_Tag_));
+			} while (__ptr != __aligned_end);
+
+			__seek_possibly_wrapped_iterator(_iterator, __ptr);
+
+			if (_iterator == _sentinel)
+				return;
+
+			for (auto __next = _iterator; ++__next != _sentinel; _iterator = __next)
+				if (_predicate(_proj(*_iterator), _proj(*__next)))
+					break;
+		}
+
+		template <vx::simd_type _Tag_, sizetype _AlignedSize_, sizetype _TailSize_>
+		raze_nodiscard raze_always_inline void operator()(_Tag_, 
+			std::integral_constant<sizetype, _AlignedSize_>,
+			std::integral_constant<sizetype, _TailSize_>) noexcept 
+		{
+			constexpr auto __iterations_aligned = _AlignedSize_ / sizeof(_Tag_);
+
+			auto* __ptr = std::to_address(_iterator);
+			auto* __end = __bytes_pointer_offset(__ptr, _AlignedSize_ + _TailSize_);
+
+			raze_assume(__ptr != nullptr);
+
+			auto __next_ptr = __bytes_pointer_offset(__ptr, sizeof(typename _Tag_::value_type));
+			auto __left = __iterations_aligned;
+
+			do {
+				const auto __current = _proj(raze::vx::load<_Tag_>(__ptr));
+				const auto __next = _proj(raze::vx::load<_Tag_>(__next_ptr));
+				
+				const auto __mask = _predicate(__current, __next);
+
+				if (raze::vx::any_of(__mask)) {
+					__seek_possibly_wrapped_iterator(_iterator, __ptr + raze::vx::find_first_set(__mask));
+					return;
+				}
+
+				__advance_bytes(__ptr, sizeof(_Tag_));
+				__advance_bytes(__next_ptr, sizeof(_Tag_));
+			} while (--__left);
+			
+			__seek_possibly_wrapped_iterator(_iterator, __ptr);
+
+			if constexpr (_TailSize_ == 0)
+				return;
+
+			for (auto __next = _iterator; ++__next != _sentinel; _iterator = __next)
+				if (_predicate(_proj(*_iterator), _proj(*__next)))
+					break;
+		}
+
+		raze_nodiscard constexpr raze_always_inline _Iterator_ result() const noexcept {
+			return _iterator;
+		}
+	};
+
+	template <std::input_iterator _Iterator_, std::sentinel_for<_Iterator_> _Sentinel_,
+		class _Predicate_ = std::equal_to<>, class _Projection_ = std::identity>
+	raze_nodiscard constexpr raze_always_inline _Iterator_ operator()(_Iterator_ __first,
+		_Sentinel_ __last, _Predicate_ __pred = {}, _Projection_ __proj = {}) const noexcept
+	{
+		__seek_possibly_wrapped_iterator(__first, __adjacent_find_unchecked(
+			type_traits::__ranges_unwrap_iterator<_Sentinel_>(std::move(__first)),
+			type_traits::__ranges_unwrap_sentinel<_Iterator_>(std::move(__last)),
+			type_traits::__pass_function(__pred), type_traits::__pass_function(__proj)));
+
+		return __first;
+	}
+
+	template <std::ranges::input_range _Range_, class _Predicate_ = std::equal_to<>, class _Projection_ = std::identity>
+	constexpr raze_always_inline std::ranges::borrowed_iterator_t<_Range_> operator()(
+		_Range_&& __range, _Predicate_ __pred = {}, _Projection_ __proj = {}) const noexcept
+			requires(!constexpr_sized_range<_Range_>)
+	{
+		return __adjacent_find_unchecked(std::ranges::begin(__range), std::ranges::end(__range),
+			type_traits::__pass_function(__pred), type_traits::__pass_function(__proj));
+	}
+
+	template <std::ranges::input_range _Range_, class _Predicate_ = std::equal_to<>, class _Projection_ = std::identity>
+	constexpr raze_always_inline std::ranges::borrowed_iterator_t<_Range_> operator()(_Range_&& __range,
+		_Predicate_ __pred = {}, _Projection_ __proj = {}) const noexcept
+			requires(constexpr_sized_range<_Range_>)
+	{
+		return __adjacent_find_unchecked(std::ranges::begin(__range), std::ranges::end(__range),
+			type_traits::__pass_function(__pred), type_traits::__pass_function(__proj), 
+			std::integral_constant<sizetype, __range_constexpr_size<_Range_>()>{});
+	}
+private:
+	template <class _Iterator_, class _Sentinel_, class _Predicate_, class _Projection_>
+	raze_nodiscard constexpr raze_always_inline _Iterator_ __adjacent_find_unchecked(
+		_Iterator_ __first, _Sentinel_ __last, _Predicate_ __pred, _Projection_ __proj) const noexcept
+	{
+		using _Value_ = std::iter_value_t<_Iterator_>;
+
+		__verify_range(__first, __last);
+		auto __work = __impl(__first, __last, __pred, __proj);
+		
+		if constexpr (std::contiguous_iterator<_Iterator_> && vectorizable_unary_predicate<_Predicate_, _Iterator_> &&
+			vectorizable_projection<_Projection_, _Iterator_>)
+		{
+			if not consteval {
+				__seek_possibly_wrapped_iterator(__first, vx::__dispatch_sized_impl<typename options::_Unroller<decltype(this->traits())>::__impl,
+					_Value_, _Iterator_>(algorithm::distance(__first, __last) * sizeof(_Value_), __work));
+				return __first;
+			}
+		}
+		
+		__seek_possibly_wrapped_iterator(__first, options::__unroller<decltype(this->traits()), vx::scalar_tag>(__work));
+		return __first;
+	}
+
+	template <class _Iterator_, class _Sentinel_, class _Predicate_, class _Projection_, sizetype _Size_>
+	raze_nodiscard constexpr raze_always_inline _Iterator_ __adjacent_find_unchecked(_Iterator_ __first,
+		_Sentinel_ __last, _Predicate_ __pred, _Projection_ __proj, std::integral_constant<sizetype, _Size_> __size) const noexcept
+	{
+		using _Value_ = std::iter_value_t<_Iterator_>;
+
+		__verify_range(__first, __last);
+		auto __work = __impl(__first, __last, __pred, __proj);
+
+		if constexpr (std::contiguous_iterator<_Iterator_> && vectorizable_unary_predicate<_Predicate_, _Iterator_>
+			&& vectorizable_projection<_Projection_, _Iterator_>)
+		{
+			if not consteval {
+				__seek_possibly_wrapped_iterator(__first, vx::__dispatch_sized_impl<typename options::_Unroller<decltype(this->traits())>::__impl,
+					_Value_, _Iterator_>(std::integral_constant<sizetype, _Size_ * sizeof(_Value_)>{}, __work));
+				return __first;
+			}
+		}
+		
+		__seek_possibly_wrapped_iterator(__first, options::__unroller<decltype(this->traits()), vx::scalar_tag>(__work));
+		return __first;
+	}
+};
+
+constexpr inline auto adjacent_find = raze::options::function_with_traits<_Adjacent_find>;
 
 __RAZE_ALGORITHM_NAMESPACE_END
--- tests/algorithm/AdjacentFindTest.cpp
+++ tests/algorithm/AdjacentFindTest.cpp
@@ -2,17 +2,22 @@
 #include <cassert>
 #include <vector>
 #include <algorithm>
+#include <ranges>
 #include <iostream>
 #include <random>
 #include <cstdint>
 #include <type_traits>
+#include <cmath>
+#include <cstdlib>
+#include <functional>
 
 #include <raze/algorithm/find/AdjacentFind.h>
+#include <raze/math/Math.h>
 
 template <class It>
 void check_adjacent(It first, It last)
 {
-    auto std_res = std::adjacent_find(first, last);
+    auto std_res = std::ranges::adjacent_find(first, last);
     auto simd_res = raze::algorithm::adjacent_find(first, last);
     raze_assert(std_res == simd_res);
 }
@@ -20,93 +25,187 @@ void check_adjacent(It first, It last)
 template <class It, class Pred>
 void check_adjacent(It first, It last, Pred pred)
 {
-    auto std_res = std::adjacent_find(first, last, pred);
+    auto std_res = std::ranges::adjacent_find(first, last, pred);
     auto simd_res = raze::algorithm::adjacent_find(first, last, pred);
     raze_assert(std_res == simd_res);
 }
+
+template <class It, class Pred, class Proj>
+void check_adjacent(It first, It last, Pred pred, Proj proj)
+{
+    auto std_res = std::ranges::adjacent_find(first, last, pred, proj);
+    auto simd_res = raze::algorithm::adjacent_find(first, last, pred, proj);
+    raze_assert(std_res == simd_res);
+}
+
+template <class Rng>
+void check_adjacent_range(Rng& rng)
+{
+    auto std_res = std::ranges::adjacent_find(rng);
+    auto simd_res = raze::algorithm::adjacent_find(rng); 
+    raze_assert(std_res == simd_res);
+}
+
+template <class Rng, class Pred>
+void check_adjacent_range(Rng& rng, Pred pred)
+{
+    auto std_res = std::ranges::adjacent_find(rng, pred);
+    auto simd_res = raze::algorithm::adjacent_find(rng, pred);
+    raze_assert(std_res == simd_res);
+}
+
+template <class Rng, class Pred, class Proj>
+void check_adjacent_range(Rng& rng, Pred pred, Proj proj)
+{
+    auto std_res = std::ranges::adjacent_find(rng, pred, proj);
+    auto simd_res = raze::algorithm::adjacent_find(rng, pred, proj);
+    raze_assert(std_res == simd_res);
+}
+
+struct Item {
+    int id;
+    int value;
+};
 
 template <class T>
 void run_tests_for_type()
 {
     {
-        std::vector<T> v = { 1,2,3,4,5 };
-        check_adjacent(v.begin(), v.end());
-    }
-    {
-        std::vector<T> v = { 1,1,2,3,4 };
-        check_adjacent(v.begin(), v.end());
-    }
-    {
-        std::vector<T> v = { 5,4,3,2,1,1 };
-        check_adjacent(v.begin(), v.end());
-    }
-    {
-        std::vector<T> v = { 7,7,7,7 };
-        check_adjacent(v.begin(), v.end());
-    }
-    {
         std::vector<T> v = {};
         check_adjacent(v.begin(), v.end());
+        check_adjacent_range(v);
+        
+        check_adjacent(v.begin(), v.end(), std::greater<>{});
+        check_adjacent_range(v, std::greater<>{});
     }
     {
-        std::vector<T> v = { 42 };
+        std::vector<T> v = { T(42) };
         check_adjacent(v.begin(), v.end());
+        check_adjacent_range(v);
+        
+        check_adjacent(v.begin(), v.end(), std::less<>{});
+        check_adjacent_range(v, std::less<>{});
+    }
+
+    {
+        std::vector<T> v = { T(1), T(2), T(3), T(4), T(5) };
+        check_adjacent(v.begin(), v.end());
+        check_adjacent_range(v);
     }
     {
-        if constexpr (!std::is_floating_point_v<T>) {
-            std::vector<T> v = { 1,2,3,4,5,6 };
-            auto pred = [](T a, T b) { return (a % 2) == (b % 2); };
-            check_adjacent(v.begin(), v.end(), pred);
-        }
+        std::vector<T> v = { T(1), T(1), T(2), T(3), T(4) };
+        check_adjacent(v.begin(), v.end());
+        check_adjacent_range(v);
     }
     {
-        std::vector<T> v = { 1,3,5,7,9 };
-        auto pred = [](T a, T b) { return true; };
+        std::vector<T> v = { T(-1), T(2), T(-5), T(5), T(3)};
+        auto proj = [](T x) { return raze::math::abs(x); };
+        check_adjacent(v.begin(), v.end(), std::equal_to<>{}, proj);
+        check_adjacent_range(v, std::equal_to<>{}, proj);
+    }
+    {
+        std::vector<T> v = { T(1), T(3), T(2), T(4), T(5) };
+        check_adjacent(v.begin(), v.end(), std::greater<>{});
+        check_adjacent_range(v, std::greater<>{});
+    }
+    {
+        std::vector<T> v = { T(10), T(20), T(21), T(30) };
+        auto pred = [](T a, T b) { return raze::math::abs(a - b) < 2; };
         check_adjacent(v.begin(), v.end(), pred);
+        check_adjacent_range(v, pred);
     }
 
     for (std::size_t n = 1; n < 200; ++n) {
         std::vector<T> v(n);
         for (std::size_t i = 0; i < n; ++i)
             v[i] = T(i % 5);
-        if (n == 66)
-            n = 66;
         check_adjacent(v.begin(), v.end());
+        check_adjacent_range(v);
     }
 
     {
-        const std::size_t N = 1'000'000;
+        const std::size_t N = 100'00;
         std::vector<T> v(N);
         for (std::size_t i = 0; i < N; ++i)
             v[i] = T(i % 13);
         check_adjacent(v.begin(), v.end());
+        check_adjacent_range(v);
     }
 
     {
         std::mt19937_64 rng(123456);
-        std::uniform_int_distribution<uint64_t> dist_length(0, 10000);
-        std::uniform_int_distribution<uint64_t> dist_int;
+        std::uniform_int_distribution<std::size_t> dist_length(0, 5000);
+        std::uniform_int_distribution<int> dist_int(-1000, 1000);
         std::uniform_real_distribution<double> dist_fp(-1000.0, 1000.0);
 
-        for (int iter = 0; iter < 1'000'000; ++iter) {
+        for (int iter = 0; iter < 100'00; ++iter) {
             const std::size_t N = dist_length(rng);
             std::vector<T> v(N);
 
             if constexpr (std::is_floating_point_v<T>) {
-                for (std::size_t i = 0; i < N; ++i)
-                    v[i] = T(dist_fp(rng));
+                for (std::size_t i = 0; i < N; ++i) v[i] = T(dist_fp(rng));
+            } else {
+                for (std::size_t i = 0; i < N; ++i) v[i] = T(dist_int(rng));
             }
-            else {
-                for (std::size_t i = 0; i < N; ++i)
-                    v[i] = T(dist_int(rng));
-            }
+
             check_adjacent(v.begin(), v.end());
+            check_adjacent_range(v);
+
+            check_adjacent(v.begin(), v.end(), std::greater<>{});
+            check_adjacent_range(v, std::greater<>{});
+
+            if constexpr (std::is_floating_point_v<T>) {
+                auto pred_close = [](T a, T b) { return raze::math::abs(a - b) < 5.0; };
+                check_adjacent(v.begin(), v.end(), pred_close);
+                check_adjacent_range(v, pred_close);
+            } else {
+                auto pred_close = [](T a, T b) { return raze::math::abs(a - b) < 5; };
+                check_adjacent(v.begin(), v.end(), pred_close);
+                check_adjacent_range(v, pred_close);
+            }
+
+            if constexpr (!std::is_unsigned_v<T>) {
+                auto proj_abs = [](T x) { return raze::math::abs(x); };
+                check_adjacent(v.begin(), v.end(), std::equal_to<>{}, proj_abs);
+                check_adjacent_range(v, std::equal_to<>{}, proj_abs);
+            }
         }
     }
 }
 
-int main()
+void run_tests_for_struct()
 {
+    std::mt19937_64 rng(987654);
+    std::uniform_int_distribution<std::size_t> dist_length(0, 5000);
+    std::uniform_int_distribution<int> dist_val(0, 100);
+
+    for (int iter = 0; iter < 10'00; ++iter) {
+        const std::size_t N = dist_length(rng);
+        std::vector<Item> v(N);
+        
+        for (std::size_t i = 0; i < N; ++i) {
+            v[i] = { int(i % 10), dist_val(rng) };
+        }
+
+        auto get_id = [](const Item& it) { return it.id; };
+        auto get_val = [](const Item& it) { return it.value; };
+
+        check_adjacent(v.begin(), v.end(), std::equal_to<>{}, get_id);
+        check_adjacent_range(v, std::equal_to<>{}, get_id);
+
+        check_adjacent(v.begin(), v.end(), std::equal_to<>{}, get_val);
+        check_adjacent_range(v, std::equal_to<>{}, get_val);
+
+        auto pred_same_parity = [](int a, int b) { return (a % 2) == (b % 2); };
+        check_adjacent(v.begin(), v.end(), pred_same_parity, get_val);
+        check_adjacent_range(v, pred_same_parity, get_val);
+        
+        check_adjacent(v.begin(), v.end(), std::greater<>{}, get_id);
+        check_adjacent_range(v, std::greater<>{}, get_id);
+    }
+}
+
+int main() {
     run_tests_for_type<int8_t>();
     run_tests_for_type<uint8_t>();
     run_tests_for_type<int16_t>();
@@ -115,7 +214,11 @@ int main()
     run_tests_for_type<uint32_t>();
     run_tests_for_type<int64_t>();
     run_tests_for_type<uint64_t>();
+    
     run_tests_for_type<float>();
     run_tests_for_type<double>();
+    
+    run_tests_for_struct();
+    
     return 0;
 }
